4981c3d55da4144b0b58878d2d66d095576b9938
Amit Upadhyay <upadhyay@gmail.com>
2023-05-17 13:39:56 +0530

    recursive calls work

--- 02-export-host-function/guest/src/lib.rs
+++ 02-export-host-function/guest/src/lib.rs
@@ -1,5 +1,6 @@
 extern "C" {
     fn from_host();
+    fn from_host2() -> i32;
 }
 
 #[no_mangle]
@@ -7,5 +8,12 @@ pub extern "C" fn sum(x: i32) -> i32 {
     x + unsafe {
         from_host();
         10
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn foo() -> i32 {
+    unsafe {
+        from_host2()
     }
 }
--- 02-export-host-function/host/src/main.rs
+++ 02-export-host-function/host/src/main.rs
@@ -11,15 +11,24 @@ fn main() -> anyhow::Result<()> {
     // this case we're using `4` for.
     let mut store = wasmtime::Store::new(&engine, ());
 
-    let from_host = wasmtime::Func::wrap(&mut store, |_caller: wasmtime::Caller<'_, ()>| {
+    let from_host = wasmtime::Func::wrap(&mut store, |mut caller: wasmtime::Caller<'_, ()>| {
         println!("called from wasm");
+        let mut o = wasmtime::Val::I32(0);
+        let mut v = vec![o];
+        caller.get_export("foo").expect("foo not found").into_func().expect("foo not a func").call(&mut caller, &[], &mut v).expect("call failed");
+        dbg!(v);
+    });
+
+    let from_host2 = wasmtime::Func::wrap(&mut store, |mut _caller: wasmtime::Caller<'_, ()>| {
+        println!("called from wasm2");
+        10
     });
 
     // Instantiation of a module requires specifying its imports and then
     // afterwards we can fetch exports by name, as well as asserting the
     // type signature of the function with `get_typed_func`.
-    let instance = wasmtime::Instance::new(&mut store, &module, &[from_host.into()])?;
-    let sum = instance.get_typed_func::<(i32,), i32, _>(&mut store, "sum")?;
+    let instance = wasmtime::Instance::new(&mut store, &module, &[from_host.into(), from_host2.into()])?;
+    let sum = instance.get_typed_func::<(i32,), i32>(&mut store, "sum")?;
 
     // And finally we can call the wasm!
     println!("wasm said: {}", sum.call(&mut store, (1,))?);
